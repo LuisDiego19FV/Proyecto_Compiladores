357ea6c65296b7be08d34a859bf3ccbec5dc9908
Diego <luisdiegofv97@gmail.com>
2020-03-18 15:21:21 -0600

    Update F/1

--- Lexer/lexer.py
+++ Lexer/lexer.py
@@ -1,3 +1,4 @@
+import copy
 from nodes import DecompositionTree as dt
 from nodes import DecompositionTreeDFA as dfadt
 from nodes import NFATree as nt
@@ -792,3 +793,110 @@ def rootToDFA(root, alphabet):
     
     return nodes_dfa
 
+def compareTransition(transitions1, transitions2):
+    to_get = len(transitions1)
+    got = 0
+
+    for i in transitions1:
+        for j in transitions2:
+            if str(i) == str(j):
+                got += 1
+                break
+
+    return to_get == got
+
+def recursivIn(val, doubleArray):
+    for i in doubleArray:
+        for j in i:
+            if val == j:
+                return True
+
+    return False
+
+
+def recursivIndex(val, doubleArray):
+    for i in range(len(doubleArray)):
+        for j in doubleArray[i]:
+            if val == j:
+                return i
+    
+    return 0.5
+
+def nodesSort(nodes):
+    newNodes = [None] * len(nodes)
+    for i in nodes:
+        pos = int(i.getState()[1:])
+        newNodes[pos] = i
+
+    return newNodes
+
+def minimization(nodes):
+    
+    F = []
+    S = []
+
+    for i in nodes:
+        if i.getIsAcceptanceState():
+            F.append(i)
+        else:
+            S.append(i)
+
+    II = [S, F]
+    II_new = []
+
+    while II != II_new:
+
+        for G in II:
+            for i in range(len(G)):
+                
+                if recursivIn(G[i], II_new):
+                    continue
+
+                new_subgroup = [G[i]]
+                for j in range(i+1,len(G)):
+                    if compareTransition(G[i].getTransition(), G[j].getTransition()):
+                        new_subgroup.append(G[j])
+                
+                II_new.append(new_subgroup)
+
+        if II != II_new:
+            II = II_new
+            II_new = []
+
+    nodes_dfa = []
+    counter = 0
+
+    for i in II:
+
+        counter += 1
+        tmp_node = dfat("T" + str(counter))
+
+        for j in i:
+            if j.getState() == "T0":
+                counter -= 1
+                tmp_node.setState("T0")
+            if j.getIsAcceptanceState():
+                tmp_node.setIsAcceptanceState()
+        
+        for transition in i[0].getTransition():
+            to = recursivIndex(transition[0], II)
+            by = transition[1]
+            tmp_node.setTransition(to,by)
+
+        nodes_dfa.append(tmp_node)
+
+    for i in nodes_dfa:
+        new_transitions = []
+        for transition in i.getTransition():
+            to = nodes_dfa[transition[0]]
+            by = transition[1]
+            new_transitions.append((to,by))
+        
+        i.transitions = []
+        for j in new_transitions:
+            i.setTransition(j[0], j[1])
+
+    nodes_dfa = nodesSort(nodes_dfa)
+
+    
+    return nodes_dfa
--- Lexer/main.py
+++ Lexer/main.py
@@ -1,3 +1,4 @@
+import time
 import lexer as lex
 import automataPrinter as autPrint
 
@@ -14,25 +15,44 @@ rootDFA = lex.exprToDecompTreeDFA(expr)
 # Paso 3: Arbol a NFA
 nodes_nfa1 = lex.rootToNFA(rootNFA)
 autPrint.printNFA(nodes_nfa1, regex + "_nfa_1")
-print("NFA 1 completado")
+print("NFA 1 completado (Thompson)")
 # Paso 4: NFA to DFA
 nodes_dfa1 = lex.fromNFAtoDFA(nodes_nfa1, alphabet)
 autPrint.printNFA(nodes_dfa1, regex + "_dfa_1")
-print("DFA 1 completado")
+print("DFA 1 completado (NFA->DFA)")
 # Paso 5: Arbol a DFA
 nodes_dfa2 = lex.rootToDFA(rootDFA, alphabet)
 autPrint.printNFA(nodes_dfa2, regex + "_dfa_2")
-print("DFA 2 completado")
+print("DFA 2 completado (Directo)")
+# Paso 6: DFA a DFA Optimo
+nodes_dfa3 = lex.minimization(nodes_dfa1)
+autPrint.printNFA(nodes_dfa3, regex + "_dfa_3")
+print("DFA 3 completado (Optimizacion)")
+
 
 # Prueba del Automatas
 while True:
-    word = input("Ingrese palabra a verificar: ")
+    word = input("\nIngrese palabra a verificar: ")
     if word == "quit":
         break
+    # NFA 1
+    start1 = time.time()
     result1 = lex.simulateNFA(word, nodes_nfa1)
+    end1 = time.time()
+    # DFA 1
+    start2 = time.time()
     result2 = lex.simulateDFA(word, nodes_dfa1)
+    end2 = time.time()
+    # DFA 2
+    start3 = time.time()
     result3 = lex.simulateDFA(word, nodes_dfa2)
-    print("Segun el nfa 1 " + result1 + " pertenece.")
-    print("Segun el dfa 1 " + result2 + " pertenece.")
-    print("Segun el dfa 2 " + result3 + " pertenece.")
+    end3 = time.time()
+    # DFA 3
+    start4 = time.time()
+    result4 = lex.simulateDFA(word, nodes_dfa3)
+    end4 = time.time()
+    print("Segun el nfa 1 " + result1 + " pertenece. Time: " + str(end1 - start1) + "s")
+    print("Segun el dfa 1 " + result2 + " pertenece. Time: " + str(end2 - start2) + "s")
+    print("Segun el dfa 2 " + result3 + " pertenece. Time: " + str(end3 - start3) + "s")
+    print("Segun el dfa 3 " + result4 + " pertenece. Time: " + str(end4 - start4) + "s")
     
